25c20be0bdbd7ab6853faf07f72b8da4ff9c1447
Muhammad Ali Abed <abedcodecom@gmail.com>
2021-10-28 00:03:30 +0330

    & evaluating scripts in the page context &   saving extracted words to a file

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const puppeteer = require('puppeteer')
+const fs = require('fs').promises
 
 async function launchChrome() {
     const chrome =  await puppeteer.launch();
     const page = await chrome.newPage();
-    await page.goto("https://en.wikipedia.org/wiki/JavaScript")
-    await page.screenshot({path: "javascript-wikipedia.png", fullPage: true})
+    await page.goto("https://learnwebcode.github.io/practice-requests/")
+    
+    const names = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll(".info strong")).map(strong => strong.textContent)
+    });
+    await fs.writeFile('storage/names.txt', names.join("\r\n"))
 
     await chrome.close()
 }
