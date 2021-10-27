8def3119c4f6fa6d117d58ee13d273430d90874b
Muhammad Ali Abed <abedcodecom@gmail.com>
2021-10-27 22:50:54 +0330

    & fullpage screenshot example for long webpages

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const puppeteer = require('puppeteer')
 async function launchChrome() {
     const chrome =  await puppeteer.launch();
     const page = await chrome.newPage();
-    await page.goto("https://learnwebcode.github.io/practice-requests/")
-    await page.screenshot({path: "amazing.png"})
+    await page.goto("https://en.wikipedia.org/wiki/JavaScript")
+    await page.screenshot({path: "javascript-wikipedia.png", fullPage: true})
 
     await chrome.close()
 }
