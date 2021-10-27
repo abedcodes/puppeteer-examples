7de2dbe3793bcb97fb61e555c2a6beaf9d4880ab
Muhammad Ali Abed <abedcodecom@gmail.com>
2021-10-28 00:29:43 +0330

    & downloading all images of the page example

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ async function launchChrome() {
     const page = await chrome.newPage();
     await page.goto("https://learnwebcode.github.io/practice-requests/")
     
-    const names = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".info strong")).map(strong => strong.textContent)
-    });
-    await fs.writeFile('storage/names.txt', names.join("\r\n"))
+    const photos = await page.$$eval('img', imgs => {
+        return imgs.map(img => img.src)
+    })
+
+    for (const photo of photos) {
+        const imagepage = await page.goto(photo)
+        await fs.writeFile("storage/"+photo.split("/").pop(), await imagepage.buffer())
+    }
 
     await chrome.close()
 }
