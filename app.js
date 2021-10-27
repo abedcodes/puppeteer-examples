a13222e38561aba4b7234b5573029e0cb5b75f9d
Muhammad Ali Abed <abedcodecom@gmail.com>
2021-10-28 01:01:23 +0330

    & simulating click & evaluating the page afterward

--- app.js
+++ app.js
@@ -6,14 +6,9 @@ async function launchChrome() {
     const page = await chrome.newPage();
     await page.goto("https://learnwebcode.github.io/practice-requests/")
     
-    const photos = await page.$$eval('img', imgs => {
-        return imgs.map(img => img.src)
-    })
-
-    for (const photo of photos) {
-        const imagepage = await page.goto(photo)
-        await fs.writeFile("storage/"+photo.split("/").pop(), await imagepage.buffer())
-    }
+    await page.click("#clickme")
+    const data = await page.$eval("#data", el => el.textContent)
+    console.log(data);
 
     await chrome.close()
 }
